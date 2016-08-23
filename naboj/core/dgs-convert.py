365d8b59b919d295ea78e8772cb72a3b6e805551
Kvík <martin.balaz@heh.sk>
2016-08-23 18:20:40 +0200

    Monster pack again :-(

--- naboj/core/dgs-convert.py
+++ naboj/core/dgs-convert.py
@@ -5,17 +5,25 @@ import os, sys, re, argparse
 parser = argparse.ArgumentParser(
     description             = "DeGeŠ Náboj conversion utility",
 )
+parser.add_argument('language', choices = ['slovak', 'english'])
 parser.add_argument('infile',   nargs = '?', type = argparse.FileType('r'), default = sys.stdin)
 parser.add_argument('outfile',  nargs = '?', type = argparse.FileType('w'), default = sys.stdout) 
 args = parser.parse_args()
 
+quotes = {
+    'slovak': ('„', '“'),
+    'english': ('“', '”'),
+    'hungarian': ('„', '”'),
+}
+
 tempfile = open('.convert-temp', 'w')
 
+(qo, qc) = quotes[args.language]
 for line in args.infile:
     line = re.sub(r'^%(.*)$', '', line)
-    line = re.sub(r'^"', '„', line)
-    line = re.sub(r' "', ' „', line)
-    line = re.sub(r'"', '“', line)
+    line = re.sub(r'^"', qo, line)
+    line = re.sub(r' "', ' ' + qo, line)
+    line = re.sub(r'"', qc, line)
     tempfile.write(line)
 
 tempfile.close()
