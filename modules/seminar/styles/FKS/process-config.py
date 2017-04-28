4cc981fdb3b3afb93f0376e2bc29fd4914793389
Kvík <martin.balaz@heh.sk>
2017-04-28 17:45:43 +0000

    Tidied up the repo

--- modules/seminar/styles/FKS/process-config.py
+++
@@ -1,8 +0,0 @@
-import jinja2
-
-def processCategories():
-    for problem in config.headers.categories:
-        if len(problem) == 1:
-            problem = "kategória \\textbf{problem[0]}"
-        if len(problem) == 2:
-            problem = "kategórie \\textbf{problem[0]} a \\textbf{problem[1]}"
