701f890b5ec04afd983ed22ad43d224015f19b81
Kvík <martin.balaz@trojsten.sk>
2018-10-02 12:12:04 +0000

    Shotgun development

--- core/filters.py
+++ core/filters.py
@@ -1,36 +1,36 @@
-#!/usr/bin/env python3
-
-def roman(what):
-    what = int(what)
-    if what == 0:
+def roman(number):
+    number = int(number)
+    if number == 0:
         return '0'
-    if what > 4000:
+    if number > 4000:
         raise ValueError("Argument must be between 1 and 3999")
 
-    ints = (1000, 900,  500, 400, 100,  90, 50,  40, 10,  9,   5,  4,   1)
-    nums = ('M',  'CM', 'D', 'CD','C', 'XC','L','XL','X','IX','V','IV','I')
+    ints = (1000,  900, 500,  400, 100,   90,  50,   40,  10,    9,   5,    4,   1)
+    nums = ( 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
     result = ""
     for i in range(len(ints)):
-        count = int(what / ints[i])
+        count = int(number / ints[i])
         result += nums[i] * count
-        what -= ints[i] * count
+        number -= ints[i] * count
     return result
 
 def formatList(list):
     return renderList(list, bold = True)
 
-def renderList(what, **kwargs):
-    if (type(what) == str):
-        what = [what]
+def renderList(items, **kwargs):
+    if isinstance(items, str):
+        items = [items]
+
+    if not isinstance(items, list):
+        raise TypeError("{} is not a list".format(items))
 
     if kwargs.get('bold', False):
-        what = ['\\textbf{{{}}}'.format(x) for x in what if x != '']
+        items = ['\\textbf{{{}}}'.format(x) for x in items]
 
-    for i, item in enumerate(what[:-2]):
-        what[i] = '{},'.format(item)
+    for i, item in enumerate(items[:-2]):
+        items[i] = '{},'.format(item)
 
-    if len(what) > 1:
-        what[-2] = '{} a'.format(what[-2])
+    if len(items) > 1:
+        items[-2] = '{} a'.format(items[-2])
     
-    return ' '.join(what)
-
+    return ' '.join(items)
