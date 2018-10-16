a1884af29bcef029e43efcfb9e6bc90a5f021e33
krtko1 <krtko@fks.sk>
2018-10-16 20:25:27 +0200

    add user option to dgs-copy.py

--- dgs-copy.py
+++ dgs-copy.py
@@ -5,8 +5,8 @@ from colorama import Fore as cf
 
 colorama.init()
 
-VERSION = "1.00"
-DATE = "2016-09-11"
+VERSION = "2.00"
+DATE = "2018-10-16"
 
 parser = argparse.ArgumentParser(
     description             = "Copy a DeGeŠ round to www-archiv",
@@ -14,11 +14,12 @@ parser = argparse.ArgumentParser(
 parser.add_argument('seminar', choices = ['FKS', 'KMS', 'KSP', 'UFO', 'PRASK', 'FX'])
 parser.add_argument('volume', type = int)
 parser.add_argument('part', type = int, choices = [1, 2])
-parser.add_argument('round', type = int, choices = [1, 2, 3]) 
+parser.add_argument('round', type = int, choices = [1, 2, 3])
+parser.add_argument('user')
 
 args = parser.parse_args()
 
-query = "ssh sesquideus@ksp.sk 'mkdir -p \
+query = "ssh {user}@ksp.sk 'mkdir -p \
 /var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/zadania/html/ \
 /var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/vzoraky/html/ \
 /var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/obrazky/'".format(
@@ -26,55 +27,61 @@ query = "ssh sesquideus@ksp.sk 'mkdir -p \
     volume = args.volume,
     part = args.part,
     round = args.round,
+    user = args.user,
 )
 print(query)
 os.system(query)
 
 for problem in range(1, 8):
-    query = "scp ./output/seminar/{seminar}/{volume}/{part}/{round}/{problem:02d}/problem.html sesquideus@ksp.sk:/var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/zadania/html/prikl{problem}.html".format(
+    query = "scp ./output/seminar/{seminar}/{volume}/{part}/{round}/{problem:02d}/problem.html {user}@ksp.sk:/var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/zadania/html/prikl{problem}.html".format(
         seminar = args.seminar,
         volume = args.volume,
         part = args.part,
         round = args.round,
         problem = problem,
+        user = args.user,
     )
     print(query)
     os.system(query)
 
 for problem in range(1, 8):
-    query = "scp ./output/seminar/{seminar}/{volume}/{part}/{round}/{problem:02d}/solution.html sesquideus@ksp.sk:/var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/vzoraky/html/prikl{problem}.html".format(
+    query = "scp ./output/seminar/{seminar}/{volume}/{part}/{round}/{problem:02d}/solution.html {user}@ksp.sk:/var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/vzoraky/html/prikl{problem}.html".format(
         seminar = args.seminar,
         volume = args.volume,
         part = args.part,
         round = args.round,
         problem = problem,
-    )   
+        user = args.user,
+    )
     print(query)
     os.system(query)
 
-query = "scp ./output/seminar/{seminar}/{volume}/{part}/{round}/problems.pdf sesquideus@ksp.sk:/var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/zadania/zadania.pdf".format(
+query = "scp ./output/seminar/{seminar}/{volume}/{part}/{round}/problems.pdf {user}@ksp.sk:/var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/zadania/zadania.pdf".format(
     seminar = args.seminar,
     volume = args.volume,
     part = args.part,
     round = args.round,
+    user = args.user,
 )
 print(query)
 os.system(query)
 
-query = "scp ./output/seminar/{seminar}/{volume}/{part}/{round}/solutions.pdf sesquideus@ksp.sk:/var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/vzoraky/vzoraky.pdf".format(
+query = "scp ./output/seminar/{seminar}/{volume}/{part}/{round}/solutions.pdf {user}@ksp.sk:/var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/vzoraky/vzoraky.pdf".format(
     seminar = args.seminar,
     volume = args.volume,
     part = args.part,
     round = args.round,
+    user = args.user,
 )
 print(query)
 os.system(query)
 
-query = "find ./output/seminar/{seminar}/{volume}/{part}/{round}/ \( -name '*.jpg' -o -name '*.png' \) -exec scp '{{}}' sesquideus@ksp.sk:/var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/obrazky/ \;".format(
+query = "find ./output/seminar/{seminar}/{volume}/{part}/{round}/ \( -name '*.jpg' -o -name '*.png' \) -exec scp '{{}}' {user}@ksp.sk:/var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/obrazky/ \;".format(
     seminar = args.seminar,
     volume = args.volume,
     part = args.part,
     round = args.round,
+    user = args.user,
 )
 print(query)
 os.system(query)
