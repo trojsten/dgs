ccfba3a5ca2cf30a60dba6dc0c0ecf92a1472979
Kvík <martin.balaz@trojsten.sk>
2018-11-20 13:45:16 +0000

    Minifixes

--- modules/naboj/module.mk
+++ modules/naboj/module.mk
@@ -21,6 +21,8 @@ input/naboj/%/build-venue: \
 	@mkdir -p $(dir $@)
 	python3 modules/naboj/build-venue.py 'source/naboj/' -c $(word 1,$(words)) -v $(word 2,$(words)) -p $(word 4,$(words)) -o '$(dir $@)'
 
+################################### input ####################################
+# % <competition>/<volume>/<venue>
 input/naboj/%/tearoff.tex: \
     input/naboj/$$*/build-venue \
     $$(subst $$(cdir),,$$(abspath source/naboj/$$*/meta.yaml)) ;
@@ -61,19 +63,6 @@ input/naboj/%/pdf-prerequisites: \
 	$$(subst source/,input/,$$(subst .gp,.pdf,$$(wildcard source/naboj/$$*/*/*.gp))) \
 	$$(wildcard source/naboj/$$*/*/meta.yaml) \
 	$$(subst $$(cdir),,$$(abspath source/naboj/$$*/../../meta.yaml)) ;
-
-input/naboj/%/barcodes.txt: \
-    $$(subst $$(cdir),,$$(abspath source/naboj/$$*/meta.yaml))
-	$(eval words := $(subst /, ,$*))
-	@mkdir -p $(dir $@)
-	python3 modules/naboj/build-venue.py 'source/naboj/' -c $(word 1,$(words)) -v $(word 2,$(words)) -p $(word 4,$(words)) -o '$(dir $@)'
-
-input/naboj/%/barcodes.pdf: \
-	input/naboj/%/barcodes.txt
-	@echo -e '$(c_action)Creating barcode PDF file $(c_filename)$@$(c_action):$(c_default)'
-	barcode -e "128" -i $< -g "120x30" -p "120x30mm" -n -o $(subst .txt,.ps,$<)
-	ps2pdf $(subst .txt,.ps,$<) $@.big
-	pdfcrop $@.big $@
 
 output/naboj/%/booklet.pdf: \
 	$$(subst source/,input/,$$(subst .md,.tex,$$(wildcard source/naboj/$$*/*/problem.md))) \
@@ -117,21 +106,6 @@ output/naboj/%/answers-mod5.pdf: \
 	@echo -e '$(c_action)Compiling XeLaTeX file $(c_filename)$@$(c_action): secondary run$(c_default)'
 	@texfot xelatex -file-line-error -jobname=$(subst .pdf,,$@) -halt-on-error -interaction=nonstopmode input/naboj/$*/answers-mod5.tex
 
-output/naboj/%/tearoff.pdf: \
-	$$(subst source/,input/,$$(subst .md,.tex,$$(wildcard $$(subst $(cdir),,$$(abspath source/naboj/$$*/../../languages/*/*/problem.md))))) \
-	$$(subst source/,input/,$$(subst $(cdir),,$$(abspath $$(wildcard source/naboj/$$*/../../languages/*/*/*.jpg)))) \
-	$$(subst source/,input/,$$(subst $(cdir),,$$(abspath $$(wildcard source/naboj/$$*/../../languages/*/*/*.png)))) \
-	$$(subst source/,input/,$$(subst $(cdir),,$$(abspath $$(wildcard source/naboj/$$*/../../languages/*/*/*.pdf)))) \
-	$$(subst source/,input/,$$(subst $(cdir),,$$(abspath $$(subst .svg,.pdf,$$(wildcard source/naboj/$$*/../../languages/*/*/*.svg))))) \
-	$$(subst source/,input/,$$(subst $(cdir),,$$(abspath $$(subst .gp,.pdf,$$(wildcard source/naboj/$$*/../../languages/*/*/*.gp))))) \
-	input/naboj/%/barcodes.pdf \
-	input/naboj/%/tearoff.tex 
-	mkdir -p $(dir $@)
-	@echo -e '$(c_action)Compiling XeLaTeX file $(c_filename)$@$(c_action): primary run$(c_default)'
-	@texfot xelatex -file-line-error -jobname=$(subst .pdf,,$@) -halt-on-error -interaction=nonstopmode input/naboj/$*/tearoff.tex
-	@echo -e '$(c_action)Compiling XeLaTeX file $(c_filename)$@$(c_action): secondary run$(c_default)'
-	@texfot xelatex -file-line-error -jobname=$(subst .pdf,,$@) -halt-on-error -interaction=nonstopmode input/naboj/$*/tearoff.tex
-
 output/naboj/%/constants.pdf: \
 	input/naboj/%/constants.tex \
 	input/naboj/%/constants-table.tex \
@@ -158,6 +132,41 @@ output/naboj/%/cover.pdf: \
 	@echo -e '$(c_action)Compiling XeLaTeX file $(c_filename)$@$(c_action): primary run$(c_default)'
 	@texfot xelatex -file-line-error -jobname=$(subst .pdf,,$@) -halt-on-error -interaction=nonstopmode input/naboj/$*/cover.tex
 
+########################## venue ################################
+
+# Barcodes in text format
+input/naboj/%/barcodes.txt: \
+    $$(subst $$(cdir),,$$(abspath source/naboj/$$*/meta.yaml))
+	$(eval words := $(subst /, ,$*))
+	@mkdir -p $(dir $@)
+	python3 modules/naboj/build-venue.py 'source/naboj/' -c $(word 1,$(words)) -v $(word 2,$(words)) -p $(word 4,$(words)) -o '$(dir $@)'
+
+# Barcodes text -> PDF, one per page
+input/naboj/%/barcodes.pdf: \
+	input/naboj/%/barcodes.txt
+	@echo -e '$(c_action)Creating barcode PDF file $(c_filename)$@$(c_action):$(c_default)'
+	barcode -e "128" -i $< -g "120x30" -p "120x30mm" -n -o $(subst .txt,.ps,$<)
+	ps2pdf $(subst .txt,.ps,$<) $@.big
+	pdfcrop $@.big $@
+
+# Tearoffs, three problems per page, aligned for cutting
+output/naboj/%/tearoff.pdf: \
+	$$(subst source/,input/,$$(subst .md,.tex,$$(wildcard $$(subst $(cdir),,$$(abspath source/naboj/$$*/../../languages/*/*/problem.md))))) \
+	$$(subst source/,input/,$$(subst $(cdir),,$$(abspath $$(wildcard source/naboj/$$*/../../languages/*/*/*.jpg)))) \
+	$$(subst source/,input/,$$(subst $(cdir),,$$(abspath $$(wildcard source/naboj/$$*/../../languages/*/*/*.png)))) \
+	$$(subst source/,input/,$$(subst $(cdir),,$$(abspath $$(wildcard source/naboj/$$*/../../languages/*/*/*.pdf)))) \
+	$$(subst source/,input/,$$(subst $(cdir),,$$(abspath $$(subst .svg,.pdf,$$(wildcard source/naboj/$$*/../../languages/*/*/*.svg))))) \
+	$$(subst source/,input/,$$(subst $(cdir),,$$(abspath $$(subst .gp,.pdf,$$(wildcard source/naboj/$$*/../../languages/*/*/*.gp))))) \
+	input/naboj/%/barcodes.pdf \
+	input/naboj/%/tearoff.tex 
+	mkdir -p $(dir $@)
+	@echo -e '$(c_action)Compiling XeLaTeX file $(c_filename)$@$(c_action): primary run$(c_default)'
+	@texfot xelatex -file-line-error -jobname=$(subst .pdf,,$@) -halt-on-error -interaction=nonstopmode input/naboj/$*/tearoff.tex
+	@echo -e '$(c_action)Compiling XeLaTeX file $(c_filename)$@$(c_action): secondary run$(c_default)'
+	@texfot xelatex -file-line-error -jobname=$(subst .pdf,,$@) -halt-on-error -interaction=nonstopmode input/naboj/$*/tearoff.tex
+
+output/naboj/%/envelope.pdf: ;
+
 # All targets for <language>
 output/naboj/%/all: \
 	output/naboj/$$*/booklet-print.pdf \
@@ -169,7 +178,8 @@ output/naboj/%/all: \
 
 # All targets for <venue>
 output/naboj/%/all: \
-	output/naboj/$$*/tearoff.pdf ;
+	output/naboj/$$*/tearoff.pdf \
+    output/naboj/$$*/envelope.pdf ;
 
 # All targets for all languages
 output/naboj/%/languages/all: \
