7145b0b98812170ce567a016997220567d48c332
Kvík <martin.balaz@trojsten.sk>
2018-11-20 17:05:28 +0000

    Tidied up scholar module.mk

--- modules/scholar/module.mk
+++ modules/scholar/module.mk
@@ -3,17 +3,32 @@
 input/scholar/%/build-handout: \
 	modules/scholar/format-handout.tex \
 	modules/scholar/templates/handout.tex \
-    source/scholar/$$*/meta.yaml
+	source/scholar/$$*/meta.yaml
 	@echo -e '$(c_action)Building handout for $(c_filename)$*$(c_action):$(c_default)'
 	$(eval words := $(subst /, ,$*))
 	@mkdir -p $(dir $@)
 	python3 modules/scholar/build-handout.py 'source/scholar/' $(word 1,$(words)) $(word 2,$(words)) $(word 4,$(words)) -o '$(dir $@)'
 
+input/scholar/%/build-homework: \
+	modules/scholar/format-homework.tex \
+	modules/scholar/templates/homework.tex \
+	source/scholar/$$*/meta.yaml
+	@echo -e '$(c_action)Building homework for $(c_filename)$*$(c_action):$(c_default)'
+	$(eval words := $(subst /, ,$*))
+	@mkdir -p $(dir $@)
+	python3 modules/scholar/build-homework.py 'source/scholar/' $(word 1,$(words)) $(word 2,$(words)) $(word 4,$(words)) -o '$(dir $@)'
+
 input/scholar/%/format-handout.tex: \
 	input/scholar/$$*/build-handout ;
 
+input/scholar/%/format-homework.tex: \
+	input/scholar/$$*/build-homework ;
+
 input/scholar/%/handout.tex: \
 	input/scholar/$$*/build-handout ;
+
+input/scholar/%/homework.tex: \
+	input/scholar/$$*/build-homework ;
 
 input/scholar/%/pdf-prerequisites: \
 	$$(subst source/,input/,$$(wildcard source/scholar/$$*/*.jpg)) \
@@ -28,36 +43,13 @@ output/scholar/%/handout.pdf: \
 	input/scholar/$$*/handout.tex \
 	input/scholar/$$*/format-handout.tex \
 	input/scholar/$$*/pdf-prerequisites
-	@mkdir -p $(dir $@)
-	@echo -e '$(c_action)Compiling XeLaTeX file $(c_filename)$@$(c_action), primary run:$(c_default)'
-	@texfot xelatex -file-line-error -jobname=$(subst .pdf,,$@) -halt-on-error -interaction=nonstopmode input/scholar/$*/handout.tex
-	@echo -e '$(c_action)Compiling XeLaTeX file $(c_filename)$@$(c_action), secondary run:$(c_default)'
-	@texfot xelatex -file-line-error -jobname=$(subst .pdf,,$@) -halt-on-error -interaction=nonstopmode input/scholar/$*/handout.tex
-
-input/scholar/%/build-homework: \
-	modules/scholar/format-homework.tex \
-	modules/scholar/templates/homework.tex \
-    source/scholar/$$*/meta.yaml
-	@echo -e '$(c_action)Building homework for $(c_filename)$*$(c_action):$(c_default)'
-	$(eval words := $(subst /, ,$*))
-	@mkdir -p $(dir $@)
-	python3 modules/scholar/build-homework.py 'source/scholar/' $(word 1,$(words)) $(word 2,$(words)) $(word 4,$(words)) -o '$(dir $@)'
-
-input/scholar/%/format-homework.tex: \
-	input/scholar/$$*/build-homework ;
-
-input/scholar/%/homework.tex: \
-	input/scholar/$$*/build-homework ;
+	$(call doubletex,scholar)
 
 output/scholar/%/homework.pdf: \
 	$$(subst $$(cdir),,$$(abspath input/scholar/$$*/../../../copy-static)) \
 	$$(subst source/,input/,$$(subst .md,.tex,$$(wildcard source/scholar/$$*/*.md))) \
 	input/scholar/$$*/homework.tex \
 	input/scholar/$$*/format-homework.tex
-	@mkdir -p $(dir $@)
-	@echo -e '$(c_action)Compiling XeLaTeX file $(c_filename)$@$(c_action), primary run:$(c_default)'
-	@texfot xelatex -file-line-error -jobname=$(subst .pdf,,$@) -halt-on-error -interaction=nonstopmode input/scholar/$*/homework.tex
-	@echo -e '$(c_action)Compiling XeLaTeX file $(c_filename)$@$(c_action), secondary run:$(c_default)'
-	@texfot xelatex -file-line-error -jobname=$(subst .pdf,,$@) -halt-on-error -interaction=nonstopmode input/scholar/$*/homework.tex
+	$(call doubletex,scholar)
 
 .PHONY:
