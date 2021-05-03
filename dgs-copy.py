0adc42357d3e1407f1b5985ed87f18b2a60b41c9
Matej Novota <matej.novota@gmail.com>
2021-05-03 18:00:51 +0200

    Improve dgs-copy

--- dgs-copy.py
+++ dgs-copy.py
@@ -5,77 +5,103 @@ from colorama import Fore as cf
 
 colorama.init()
 
-VERSION = "1.00"
-DATE = "2016-09-11"
+VERSION = "2.00"
+DATE = "2018-10-16"
 
 parser = argparse.ArgumentParser(
     description             = "Copy a DeGeŠ round to www-archiv",
 )
-parser.add_argument('seminar', choices = ['FKS', 'KMS', 'KSP', 'UFO', 'PRASK', 'FX'])
+parser.add_argument('seminar', choices = ['FKS', 'KMS', 'KSP', 'UFO', 'PRASK', 'FX', 'SUSI'])
 parser.add_argument('volume', type = int)
 parser.add_argument('part', type = int, choices = [1, 2])
-parser.add_argument('round', type = int, choices = [1, 2, 3]) 
+parser.add_argument('round', choices = ['1', '2', '3', 'outdoor'])
+parser.add_argument('user')
 
 args = parser.parse_args()
 
-query = "ssh sesquideus@ksp 'rm -rf /var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/;\
-mkdir -p \
+seminar2 = 'Suši' if args.seminar == 'SUSI' else args.seminar
+
+
+query = "ssh {user}@ksp.sk 'rm -rf \
+/var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round} && mkdir -p \
 /var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/zadania/html/ \
 /var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/vzoraky/html/ \
 /var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/obrazky/'".format(
-    seminar = args.seminar,
+    seminar = seminar2,
     volume = args.volume,
     part = args.part,
-    round = args.round,
+    round = 100 if args.round == 'outdoor' else args.round,
+    user = args.user,
 )
 print(query)
-os.system(query)
+#os.system(query)
 
 for problem in range(1, 8):
-    query = "scp ./output/seminar/{seminar}/{volume}/{part}/{round}/{problem:02d}/problem.html sesquideus@ksp:/var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/zadania/html/prikl{problem}.html".format(
-        seminar=args.seminar,
-        volume=args.volume,
-        part=args.part,
-        round=args.round,
-        problem=problem,
+    query = "mkdir -p tasks/{seminar2}/{volume}/{part}/{round2}/zadania/html && ln -s $(pwd)/output/seminar/{seminar1}/{volume:02d}/{part}/{round1}/{problem:02d}/problem.html tasks/{seminar2}/{volume}/{part}/{round2}/zadania/html/prikl{problem}.html".format(
+        seminar1 = args.seminar,
+        seminar2 = seminar2,
+        volume = args.volume,
+        part = args.part,
+        round1 = args.round,
+        round2 = 100 if args.round == 'outdoor' else args.round,
+        problem = problem,
+        user = args.user,
     )
     print(query)
     os.system(query)
 
 for problem in range(1, 8):
-    query = "scp ./output/seminar/{seminar}/{volume}/{part}/{round}/{problem:02d}/solution.html sesquideus@ksp:/var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/vzoraky/html/prikl{problem}.html".format(
-        seminar=args.seminar,
-        volume=args.volume,
-        part=args.part,
-        round=args.round,
-        problem=problem,
+    query = "mkdir -p tasks/{seminar2}/{volume}/{part}/{round2}/vzoraky/html && ln -s $(pwd)/output/seminar/{seminar1}/{volume:02d}/{part}/{round1}/{problem:02d}/solution.html tasks/{seminar2}/{volume}/{part}/{round2}/vzoraky/html/prikl{problem}.html".format(
+        seminar1 = args.seminar,
+        seminar2 = seminar2,
+        volume = args.volume,
+        part = args.part,
+        round1 = args.round,
+        round2 = 100 if args.round == 'outdoor' else args.round,
+        problem = problem,
+        user = args.user,
     )
     print(query)
     os.system(query)
 
-query = "scp ./output/seminar/{seminar}/{volume}/{part}/{round}/problems.pdf sesquideus@ksp:/var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/zadania/zadania.pdf".format(
-    seminar=args.seminar,
-    volume=args.volume,
-    part=args.part,
-    round=args.round,
+query = "ln -s $(pwd)/output/seminar/{seminar1}/{volume:02d}/{part}/{round1}/problems.pdf tasks/{seminar2}/{volume}/{part}/{round2}/zadania/zadania.pdf".format(
+    seminar1 = args.seminar,
+    seminar2 = seminar2,
+    volume = args.volume,
+    part = args.part,
+    round1 = args.round,
+    round2 = 100 if args.round == 'outdoor' else args.round,
+    user = args.user,
 )
 print(query)
 os.system(query)
 
-query = "scp ./output/seminar/{seminar}/{volume}/{part}/{round}/solutions.pdf sesquideus@ksp:/var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/vzoraky/vzoraky.pdf".format(
-    seminar=args.seminar,
-    volume=args.volume,
-    part=args.part,
-    round=args.round,
+query = "ln -s $(pwd)/output/seminar/{seminar1}/{volume:02d}/{part}/{round1}/solutions.pdf tasks/{seminar2}/{volume}/{part}/{round2}/vzoraky/vzoraky.pdf".format(
+    seminar1 = args.seminar,
+    seminar2 = seminar2,
+    volume = args.volume,
+    part = args.part,
+    round1 = args.round,
+    round2 = 100 if args.round == 'outdoor' else args.round,
+    user = args.user,
 )
 print(query)
 os.system(query)
 
-query = "find ./output/seminar/{seminar}/{volume}/{part}/{round}/ \( -name '*.jpg' -o -name '*.png' \) -exec scp '{{}}' sesquideus@ksp:/var/www-archiv/trojstenweb/tasks/{seminar}/{volume}/{part}/{round}/obrazky/ \;".format(
-    seminar=args.seminar,
-    volume=args.volume,
-    part=args.part,
-    round=args.round,
+query = "mkdir -p tasks/{seminar2}/{volume}/{part}/{round2}/obrazky/ && find output/seminar/{seminar1}/{volume:02d}/{part}/{round1}/ \( -name '*.jpg' -o -name '*.png' -o -name '*.kmz' \) -exec ln -s $(pwd)/'{{}}' tasks/{seminar2}/{volume}/{part}/{round2}/obrazky/ \;".format(
+    seminar1 = args.seminar,
+    seminar2 = seminar2,
+    volume = args.volume,
+    part = args.part,
+    round1 = args.round,
+    round2 = 100 if args.round == 'outdoor' else args.round,
+    user = args.user,
+)
+print(query)
+os.system(query)
+
+query = "rsync -rzvhPL tasks {user}@ksp.sk:/var/www-archiv/trojstenweb && rm -rf tasks".format(
+    user = args.user,
 )
 print(query)
 os.system(query)
