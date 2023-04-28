d4a42b385f42970764dc5572b21d054854a7a08a
Martin ‘Kvík’ Baláž <martin.balaz@trojsten.sk>
2023-04-28 13:56:29 +0000

    Moved stuff to assets and updated templates

--- install/install.sh
+++
@@ -1,9 +0,0 @@
-rm -rf FontPro
-git clone git@github.com:sebschub/FontPro.git FontPro
-cd FontPro
-mkdir otf
-cp ../../core/fonts/MinionPro/*.otf otf/
-./scripts/makeall MinionPro
-sudo ./scripts/install
-updmap-user --enable Map=MinionPro.map
-
