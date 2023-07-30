d451b0930fd455a7f2040ee7931628dcce5d1dc2
Martin Baláž <martin.balaz@trojsten.sk>
2023-07-30 16:51:53 +0000

    Náboj in situ fixes

--- core/markdown-check.py
+++ core/markdown-check.py
@@ -5,7 +5,7 @@ import argparse, os, sys, re
 from pathlib import Path
 
 from mdcheck import check, exceptions
-from utils import colour as c
+from utilities import colour as c
 
 
 class StyleEnforcer():
