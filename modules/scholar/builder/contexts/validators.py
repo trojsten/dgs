198d48e21874986824ae5db2993a714843312c56
TomasVoros <tomas.voros@trojsten.sk>
2024-09-27 11:13:25 +0200

    scholar validator

--- modules/scholar/builder/contexts/validators.py
+++ modules/scholar/builder/contexts/validators.py
@@ -1,15 +1,15 @@
 from enschema import Schema, Optional, Regex
 
-from core.builder.validator import FileSystemValidator, File
+from core.builder.validator import FileSystemValidator, File, FileOrLink
 
 
 class ScholarValidator(FileSystemValidator):
     _schema = Schema({
-        Optional(Regex(r'[\w-]+\.(png|jpg|svg|gp|py|dat)')): File,
+        Optional(Regex(r'[\w-]+\.(png|jpg|svg|gp|py|dat)')): FileOrLink,
         Optional(Regex(r'[\w-]+')): {
             Optional('problem.md'): File,
             Optional('solution.md'): File,
-            Optional(Regex(r'[\w-]+\.(png|jpg|svg|gp|py|dat)')): File,
+            Optional(Regex(r'[\w-]+\.(png|jpg|svg|gp|py|dat)')): FileOrLink,
         },
         Optional('text.md'): File,
         'meta.yaml': File,
