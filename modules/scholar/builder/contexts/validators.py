3968039d243de3c7f1587d6eaa93aeb6ffa327d8
TomasVoros <tomas.voros@trojsten.sk>
2024-10-08 11:07:17 +0200

    scholar validator and dawn of teams era

--- modules/scholar/builder/contexts/validators.py
+++ modules/scholar/builder/contexts/validators.py
@@ -5,11 +5,18 @@ from core.builder.validator import FileSystemValidator, File, FileOrLink
 
 class ScholarValidator(FileSystemValidator):
     _schema = Schema({
-        Optional(Regex(r'[\w-]+\.(png|jpg|svg|gp|py|dat)')): FileOrLink,
+        Optional(Regex(r'[\w-]+\.(png|jpg|svg|gp|py|dat|tsv)')): FileOrLink,
         Optional(Regex(r'[\w-]+')): {
             Optional('problem.md'): File,
             Optional('solution.md'): File,
-            Optional(Regex(r'[\w-]+\.(png|jpg|svg|gp|py|dat)')): FileOrLink,
+            'meta.yaml': File,
+            Optional(Regex(r'[\w-]+\.(png|jpg|svg|gp|py|dat|tsv)')): FileOrLink,
+            Optional(Regex(r'[\w-]+')): {
+                Optional('problem.md'): File,
+                Optional('solution.md'): File,
+                'meta.yaml': File,
+                Optional(Regex(r'[\w-]+\.(png|jpg|svg|gp|py|dat|tsv)')): FileOrLink,
+            },
         },
         Optional('text.md'): File,
         'meta.yaml': File,
